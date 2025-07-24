94a337165fe09e8343cca4575e30651e76d0d976
vachg <vachgen@yandex.ru>
2025-07-24 15:01:55 +0300

    Создание фильтр-пакета, в общим интерфейсом

---
+++ src/main/java/com/gridnine/testing/filters/FlightFilter.java
@@ -0,0 +1,8 @@
+package com.gridnine.testing.filters;
+
+import com.gridnine.testing.model.Flight;
+import java.util.List;
+
+public interface FlightFilter {
+    List<Flight> filter(List<Flight> flights);
+} 
